ef3f9db90808dfcde69d508a5466be13a3045722
garcey <2224529658@qq.com>
2025-04-02 18:27:08 +0800

    图片更改

--- src/views/QualityPrediction.vue
+++ src/views/QualityPrediction.vue
@@ -49,16 +49,16 @@ export default {
   data() {
     return {
       moduleTemplates: [
-        { name: '数据导入', icon: '/img/1.png', type: 'dataImport' },
-        { name: '变量选择', icon: '/img/2.png', type: 'variableSelect' },
-        { name: '异常处理', icon: '/img/3.png', type: 'exceptionHandling' },
-        { name: '标准化', icon: '/img/4.png', type: 'standardization' },
-        { name: '数据可视化', icon: '/img/5.png', type: 'dataVisualization' },
-        { name: '特征选择', icon: '/img/6.png', type: 'featureSelection' },
-        { name: '特征提取', icon: '/img/7.png', type:'featureExtraction' },
-        { name: 'RNN', icon: '/img/8.png', type:'RNN' },
-        { name: 'TCN', icon: '/img/9.png', type:'TCN' },
-        { name: 'LSTM', icon: '/img/10.png', type:'LSTM' },
+        { name: '数据导入', icon: '/img/2.png', type: 'dataImport' },
+        { name: '变量选择', icon: '/img/4.png', type: 'variableSelect' },
+        { name: '异常处理', icon: '/img/1.png', type: 'exceptionHandling' },
+        { name: '标准化', icon: '/img/5.png', type: 'standardization' },
+        { name: '数据可视化', icon: '/img/6.png', type: 'dataVisualization' },
+        { name: '特征选择', icon: '/img/7.png', type: 'featureSelection' },
+        { name: '特征提取', icon: '/img/8.png', type:'featureExtraction' },
+        { name: 'RNN', icon: '/img/9.png', type:'RNN' },
+        { name: 'TCN', icon: '/img/10.png', type:'TCN' },
+        { name: 'LSTM', icon: '/img/11.png', type:'LSTM' },
         { name: 'Transformer', icon: '/img/12.png', type:'Transformer' },
         { name: '质量预测', icon: '/img/13.png', type:'QualityPrediction' },
         // 其他模块...
